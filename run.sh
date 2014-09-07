cb695544b4248c926faae79c7cf39345c910f19c
Glen Mailer <glenjamin@gmail.com>
2014-09-07 14:02:43 +0100

    rename to non-chapter specific name

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-nodemon -w . -e "c h" -x 'sh -c' 'cc -std=c99 -Wall -Werror parsing.c mpc.c -ledit -o parsing && growlnotify -m OK || growlnotify -m FAIL'
+nodemon -w . -e "c h" -x 'sh -c' 'cc -std=c99 -Wall -Werror glenisp.c mpc.c -ledit -o glenisp && growlnotify -m OK || growlnotify -m FAIL'
